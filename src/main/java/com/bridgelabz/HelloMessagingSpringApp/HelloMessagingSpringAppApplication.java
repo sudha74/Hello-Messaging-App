ec129473d37891388737fea9c1fe50a54270e41c
sudha123 <sudhanaukala1997@gmail.com>
2023-04-07 23:29:52 +0530

    Hello Messaging App

---
+++ src/main/java/com/bridgelabz/HelloMessagingSpringApp/HelloMessagingSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.HelloMessagingSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMessagingSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/HelloMessagingSpringApp/HelloMessagingSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.HelloMessagingSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
